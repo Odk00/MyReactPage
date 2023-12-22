6782621d0e87512ca576bdbbe444ec2ff5f7c1ca
Odbayar Naranbold <odbayarnaranbold@Odbayars-Air.hsd1.il.comcast.net>
2023-12-21 20:44:23 -0600

    123

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,14 +1,22 @@
 "use client";
-import { Button, colors } from "@mui/material";
-import { addDoc } from "firebase/firestore";
+import { Button } from "@mui/material";
+import { addDoc, collection } from "firebase/firestore";
 import { useState } from "react";
+
+import { getFirestore } from "firebase/firestore";
+
+import { firebaseApp } from "@/app/firebase";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import { log } from "console";
 
 export default () => {
   const [name, setName] = useState("");
   const [hometown, setHometown] = useState("");
   const [hobby, setHobby] = useState("");
   const [favoriteColor, setFavoriteColor] = useState("");
-
+  const db = getFirestore(firebaseApp);
+  const storage = getStorage(firebaseApp);
+  const [image, setImage] = useState();
   return (
     <main>
       <h1> 12/04/2023</h1>
@@ -20,26 +28,64 @@ export default () => {
           paddingLeft: "10px",
         }}
       >
-        {JSON.stringify({
-          name: name,
-          hometown: hometown,
-          hobby: hobby,
-          favoriteColor: favoriteColor,
-        })}
         <input
           placeholder={"name"}
           onChange={(e) => {
             setName(e.target.value);
           }}
         />
-        <input placeholder={"hometown"}
-    onChange={(e) => {
-        setHometown(e.target.value)}} />
-        <input placeholder={"hobby"} onChange={(e) => {
-        setHobby(e.target.value)}}/>
-        <input placeholder={"color"} onChange={(e) => {
-        setFavoriteColor(e.target.value)}}/>
- <Button>Save</Button>
+        <input
+          placeholder={"hometown"}
+          onChange={(e) => {
+            setHometown(e.target.value);
+          }}
+        />
+        <input
+          placeholder={"hobby"}
+          onChange={(e) => {
+            setHobby(e.target.value);
+          }}
+        />
+        <input
+          placeholder={"color"}
+          onChange={(e) => {
+            setFavoriteColor(e.target.value);
+          }}
+        />
+        <Button
+          onClick={async () => {
+            const storageRef = ref(storage, image?.name);
+            await uploadBytes(storageRef, image);
+            const URL = await getDownloadURL(storageRef);
+            console.log(URL);
+          }}
+        >
+          Save Image
+        </Button>
+        <input
+          type="file"
+          onChange={(e) => {
+            setImage(e.target.files[0]);
+          }}
+        />
+        <input
+          type="file"
+          onChange={(e) => {
+            setImage(e.target.files[0]);
+          }}
+        />
+        <Button
+          onClick={async () => {
+            await addDoc(collection(db, "blog"), {
+              name: name,
+              hometown: hometown,
+              hobby: hobby,
+              favoriteColor: favoriteColor,
+            });
+          }}
+        >
+          Save
+        </Button>
       </div>
     </main>
   );
